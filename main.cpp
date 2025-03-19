11843a09d394d3175dc19878dfd767a33f3a6bff
Karl-Mitx <kescobarc14@miumg.edu.gt>
2025-03-19 15:12:51 -0600

    Estructura Basica del programa del remake de la tarea de 30 ejercicios

---
+++ main.cpp
@@ -0,0 +1,241 @@
+#include <iostream>
+using namespace std;
+
+int ejercicio1();
+int ejercicio2();
+int ejercicio3();
+int ejercicio4();
+int ejercicio5();
+int ejercicio6();
+int ejercicio7();
+int ejercicio8();
+int ejercicio9();
+int ejercicio10();
+int ejercicio11();
+int ejercicio12();
+int ejercicio13();
+int ejercicio14();
+int ejercicio15();
+int ejercicio16();
+int ejercicio17();
+int ejercicio18();
+int ejercicio19();
+int ejercicio20();
+int ejercicio21();
+int ejercicio22();
+int ejercicio23();
+int ejercicio24();
+int ejercicio25();
+int ejercicio26();
+int ejercicio27();
+int ejercicio28();
+int ejercicio29();
+int ejercicio30();
+
+int ejecutarEjercicio(int ejercicio) {
+    switch (ejercicio) {
+        case 1: return ejercicio1();
+        case 2: return ejercicio2();
+        case 3: return ejercicio3();
+        case 4: return ejercicio4();
+        case 5: return ejercicio5();
+        case 6: return ejercicio6();
+        case 7: return ejercicio7();
+        case 8: return ejercicio8();
+        case 9: return ejercicio9();
+        case 10: return ejercicio10();
+        case 11: return ejercicio11();
+        case 12: return ejercicio12();
+        case 13: return ejercicio13();
+        case 14: return ejercicio14();
+        case 15: return ejercicio15();
+        case 16: return ejercicio16();
+        case 17: return ejercicio17();
+        case 18: return ejercicio18();
+        case 19: return ejercicio19();
+        case 20: return ejercicio20();
+        case 21: return ejercicio21();
+        case 22: return ejercicio22();
+        case 23: return ejercicio23();
+        case 24: return ejercicio24();
+        case 25: return ejercicio25();
+        case 26: return ejercicio26();
+        case 27: return ejercicio27();
+        case 28: return ejercicio28();
+        case 29: return ejercicio29();
+        case 30: return ejercicio30();
+        default:
+            cout << "Este ejercicio no existe" << endl;
+        return -1;
+    }
+}
+
+
+int main() {
+    int opcion;
+    do {
+        cout << "======MENU======" << endl;
+        cout << "1. Ejecutar un ejercicio" << endl;
+        cout << "2. Salir" << endl;
+        cout << "Porfavor elija una opcion: " << endl;
+        cin >> opcion;
+
+        if (opcion == 1) {
+            int ejercicio;
+            cout << "Ingrese una opcion del 1 al 30: ";
+            cin >> ejercicio;
+            ejecutarEjercicio(ejercicio);
+        }
+    } while (opcion != 2);
+    return 0;
+}
+int ejercicio1() {
+    cout << "Ejercicio 1: " << endl;
+
+    return 0;
+}
+int ejercicio2() {
+    cout << "Ejercicio 2: " << endl;
+
+    return 0;
+}
+int ejercicio3() {
+    cout << "Ejercicio 3: " << endl;
+
+    return 0;
+}
+int ejercicio4() {
+    cout << "Ejercicio 4: " << endl;
+
+    return 0;
+}
+int ejercicio5() {
+    cout << "Ejercicio 5: " << endl;
+
+    return 0;
+}
+int ejercicio6() {
+    cout << "Ejercicio 6: " << endl;
+
+    return 0;
+}
+int ejercicio7() {
+    cout << "Ejercicio 7: " << endl;
+
+    return 0;
+}
+int ejercicio8() {
+    cout << "Ejercicio 8: " << endl;
+
+    return 0;
+}
+int ejercicio9() {
+    cout << "Ejercicio 9: " << endl;
+
+    return 0;
+}
+int ejercicio10() {
+    cout << "Ejercicio 10: " << endl;
+
+    return 0;
+}
+int ejercicio11() {
+    cout << "Ejercicio 11: " << endl;
+
+    return 0;
+}
+int ejercicio12() {
+    cout << "Ejercicio 12: " << endl;
+
+    return 0;
+}
+int ejercicio13() {
+    cout << "Ejercicio 13: " << endl;
+
+    return 0;
+}
+int ejercicio14() {
+    cout << "Ejercicio 14: " << endl;
+
+    return 0;
+}
+int ejercicio15() {
+    cout << "Ejercicio 15: " << endl;
+
+    return 0;
+}
+int ejercicio16() {
+    cout << "Ejercicio 16: " << endl;
+
+    return 0;
+}
+int ejercicio17() {
+    cout << "Ejercicio 17: " << endl;
+
+    return 0;
+}
+int ejercicio18() {
+    cout << "Ejercicio 18: " << endl;
+
+    return 0;
+}
+int ejercicio19() {
+    cout << "Ejercicio 19: " << endl;
+
+    return 0;
+}
+int ejercicio20() {
+    cout << "Ejercicio 20: " << endl;
+
+    return 0;
+}
+int ejercicio21() {
+    cout << "Ejercicio 21: " << endl;
+
+    return 0;
+}
+int ejercicio22() {
+    cout << "Ejercicio 22: " << endl;
+
+    return 0;
+}
+int ejercicio23() {
+    cout << "Ejercicio 23: " << endl;
+
+    return 0;
+}
+int ejercicio24() {
+    cout << "Ejercicio 24: " << endl;
+
+    return 0;
+}
+int ejercicio25() {
+    cout << "Ejercicio 25: " << endl;
+
+    return 0;
+}
+int ejercicio26() {
+    cout << "Ejercicio 26: " << endl;
+
+    return 0;
+}
+int ejercicio27() {
+    cout << "Ejercicio 27: " << endl;
+
+    return 0;
+}
+int ejercicio28() {
+    cout << "Ejercicio 28: " << endl;
+
+    return 0;
+}
+int ejercicio29() {
+    cout << "Ejercicio 29: " << endl;
+
+    return 0;
+}
+int ejercicio30() {
+    cout << "Ejercicio 30: " << endl;
+
+    return 0;
+}
